442c48fa42fa2b56fc7ab66625e32fb3c0b0077e
zhuguangyang <guangyang.zhu@cftcn.com>
2018-11-26 12:57:59 +0800

    游戏模块

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from  pygame.sprite import  Group
 from  settings import Settings
 from ship import Ship
 import  game_functions
@@ -10,19 +11,32 @@ def run_game():
     pygame.display.set_caption("武装飞船")
     # bg_color = (230,230,230)
 
-    ship = Ship(screen)
+    # ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
+
+    bullets = Group()
     while True:
 
         screen.fill(ai_settings.bg_color)
 
-        game_functions.check_events()
+        game_functions.check_events(ai_settings,screen,ship,bullets)
+        # bullets.update()
+        #
+        # for bullet in bullets.copy():
+        #     if bullet.rect.bottom <= 0:
+        #         bullets.remove(bullet)
+        game_functions.update_bullets(bullets)
+
+        print(len(bullets))
+
         # for event in pygame.event.get():
         #     if event.type == pygame.QUIT:
         #         sys.exit()
-
-        ship.blitme()
-
-        pygame.display.flip() #让最近绘制的屏幕可见
+        ship.update()
+        game_functions.update_screen(ai_settings,screen,ship,bullets)
+        # ship.blitme()
+        #
+        # pygame.display.flip() #让最近绘制的屏幕可见
 
 
 run_game()
