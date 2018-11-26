c08fd343f6fb3bfbd67fe12a40db38481cec05c0
zhuguangyang <guangyang.zhu@cftcn.com>
2018-11-26 18:55:10 +0800

    修改web html路径

--- PythonWeb/learning_logs/learning_log/learning_logs/views.py
+++ PythonWeb/learning_logs/learning_log/learning_logs/views.py
@@ -4,9 +4,9 @@ from  .models import Topic
 # Create your views here.
 
 def index(request):
-    return  render(request,'index.html')
+    return  render(request,'blog/index.html')
 
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics':topics}
-    return  render(request,'learning_logs/topics.html',context)
+    return  render(request,'blog/topics.html',context)
