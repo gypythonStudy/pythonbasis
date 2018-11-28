17a32de018fe4b2800beb2da8c68bdabda3592af
zhuguangyang <guangyang.zhu@cftcn.com>
2018-11-28 19:02:20 +0800

    首页model

--- PythonWeb/learning_logs/learning_log/learning_logs/models.py
+++ PythonWeb/learning_logs/learning_log/learning_logs/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+
+class GYHome(models.Model):
+
+
+    def __str__(self):
+        return self.text
+
 # Create your models here.
 class Topic(models.Model):
     text = models.CharField(max_length=200)
